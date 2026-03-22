cc4028c685474f955c978a9a75d18b26dd72407d
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-03-22 15:11:14 +0530

    [Abhay Singh Lodhi] : Added Eureka Server for microservices discovery

---
+++ eureka-server/src/test/java/com/course/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
